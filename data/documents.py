513afc5daa8b1f24ce50e6cc0536952425290269
Md Tohidul Islam <tohidul.islam@dsinnovators.com>
2023-09-01 16:16:59 +0600

    get documents code added

--- data/documents.py
+++ data/documents.py
@@ -13,3 +13,12 @@ def insert_documents(document_path_list: list):
     document_list = [Documents(document_path=document_path) for document_path in document_path_list]
     session.bulk_save_objects(document_list)
     session.commit()
+
+
+def get_documents():
+    query_result = session.query(Documents.id, Documents.document_path, Documents.summarized_document_path).all()
+    list_of_documents = []
+    for result in query_result:
+
+    return
+
