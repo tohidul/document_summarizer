70bb14524f720b70d787b6302dd31f350c34fe79
Md Tohidul Islam <tohidul.islam@dsinnovators.com>
2023-09-12 17:01:55 +0600

    commit

--- summarize.py
+++ summarize.py
@@ -4,8 +4,25 @@ from string import punctuation
 from collections import Counter
 from heapq import nlargest
 
-npl = spacy.load('en')
+nlp = spacy.load('en_core_web_sm')
 
-document = ""
+document = "Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to progressively improve their performance on a specific task. Machine learning algorithms build a mathematical model of sample data, known as “training data”, in order to make predictions or decisions without being explicitly programmed to perform the task. Machine learning algorithms are used in the applications of email filtering, detection of network intruders, and computer vision, where it is infeasible to develop an algorithm of specific instructions for performing the task. Machine learning is closely related to computational statistics, which focuses on making predictions using computers. The study of mathematical optimization delivers methods, theory and application domains to the field of machine learning. Data mining is a field of study within machine learning and focuses on exploratory data analysis through unsupervised learning. In its application across business problems, machine learning is also referred to as predictive analytics."
+
+
+def filter_tokens(doc):
+    keywords = []
+    stopwords = list(STOP_WORDS)
+    pos_tag = ["PROPN", "ADJ", "NOUN", "VERB"]
+
+    for token in doc:
+        if token.text in stopwords or token.text in punctuation:
+            continue
+        if token.pos_ in pos_tag:
+            keywords.append(token.text)
+
+    return keywords
+
 
 doc = nlp(document)
+n = len(list(doc.sents))
+print(n)
